f37eda7856affad4b03d3711c1467cd20e938e14
ALEKSEY <halturin.spb@mail.ru>
2020-07-28 14:16:55 +0300

    recursiveMethod

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.io.File;
+
 public class Main {
+    public static void main(String[] args) {
+    String folderPath = "F:\\Java";
+    File file = new File(folderPath);
+        System.out.println(getFolderSize(file));
+
+    }
+    public static long getFolderSize(File folder){
+        if(folder.isFile()){
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for(File file:files){
+            sum+=getFolderSize(file);
+        }
+        return sum;
+    }
 }

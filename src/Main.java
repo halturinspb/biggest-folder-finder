00274781a1787caf10c0e2b512c9d35cb7c71467
ALEKSEY <halturin.spb@mail.ru>
2020-07-28 17:49:40 +0300

    Complete

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
-public class Main {
-    public static void main(String[] args) {
-    String folderPath = "F:\\Java";
-    File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ParametersBag bag = new ParametersBag(args);
 
-    }
-    public static long getFolderSize(File folder){
-        if(folder.isFile()){
-            return folder.length();
-        }
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for(File file:files){
-            sum+=getFolderSize(file);
-        }
-        return sum;
+        String folderPath = bag.getPath();
+        long sizeLimit = bag.getLimit();
+
+        File file = new File(folderPath);
+        Node root = new Node(file, sizeLimit);
+
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(root);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(calculator);
+
+        System.out.println(root);
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
     }
 }
